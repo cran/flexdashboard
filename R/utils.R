bfafe15f99c5689164cf5cff23ccef27ab8c5fac
JJ Allaire <jj@rstudio.com>
2016-05-18 01:57:45 +0000

    version 0.2.0

---
+++ R/utils.R
@@ -0,0 +1,24 @@
+# return a string as a tempfile
+as_tmpfile <- function(str) {
+  if (length(str) > 0) {
+    str_tmpfile <- tempfile("rmarkdown-str", fileext = ".html")
+    writeLines(str, str_tmpfile)
+    str_tmpfile
+  } else {
+    NULL
+  }
+}
+
+# A variant of relative_to that normalizes its inputs.
+normalized_relative_to <- function(dir, file) {
+  rmarkdown::relative_to(
+    normalizePath(dir, winslash = "/", mustWork = FALSE),
+    normalizePath(file, winslash = "/", mustWork = FALSE))
+}
+
+# devel mode
+knit_devel <- function(input, ...) {
+  rmarkdown::render(input,
+                    output_options = list(devel = TRUE),
+                    quiet = TRUE)
+}
